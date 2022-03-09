0cf3f376b074772cb724e0d49c62751eafbde8ae
hungnbph <hungnb@mobio.mobio.vn>
2022-03-09 16:25:41 +0700

    ngIf ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { student, user } from './data.define';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'learn_angular';
+  public title: string;
+  public student: Array<any>;
+  public user: Array<any>;
+  public error: boolean;
+
+  constructor() {
+    this.title = 'danh sách sinh viên'
+    this.student = student();
+    this.user = user();
+    this.error = false;
+  }
+
+  handlerRemove(id: number) {
+    this.student = this.student.filter(item => item.id !== id);
+  }
+
+  handlerRemoveUser(id: number, canNang: number) {
+    if (canNang < 30) {
+      this.error = true;
+
+      return;
+    }
+    const removeUser = this.user.filter(item => item.id === id);
+  }
+
 }
---
+++ src/app/data.define.ts
@@ -0,0 +1,82 @@
+export const student = () => {
+  return [{
+    id: 1,
+    name: 'nguyễn bá hưng',
+    age: '23',
+    phone: '0338757424',
+    email: 'hung19991231@gmail.com',
+    avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1149121661142005209%2F&psig=AOvVaw00e0Otu3hJMX7KSDw4X3aD&ust=1646901467619000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDT2pnQuPYCFQAAAAAdAAAAABAD'
+  },
+  {
+    id: 2,
+    name: 'nguyễn bá hưng',
+    age: '23',
+    phone: '0338757424',
+    email: 'hung19991231@gmail.com',
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  },
+  {
+    id: 3,
+    name: 'nguyễn bá hưng',
+    age: '23',
+    phone: '0338757424',
+    email: 'hung19991231@gmail.com',
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  },
+  {
+    id: 4,
+    name: 'nguyễn bá hưng',
+    age: '23',
+    phone: '0338757424',
+    email: 'hung19991231@gmail.com',
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  },
+  {
+    id: 5,
+    name: 'nguyễn bá hưng',
+    age: '23',
+    phone: '0338757424',
+    email: 'hung19991231@gmail.com',
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  }
+  ]
+}
+
+export const user = () => {
+  return [{
+    id: 1,
+    name: 'nguyễn bá hưng',
+    canNang: 35,
+    chieuCao: 175,
+    avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1149121661142005209%2F&psig=AOvVaw00e0Otu3hJMX7KSDw4X3aD&ust=1646901467619000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDT2pnQuPYCFQAAAAAdAAAAABAD'
+  },
+  {
+    id: 2,
+    name: 'nguyễn bá hưng1',
+    canNang: 23,
+    chieuCao: 185,
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  },
+  {
+    id: 3,
+    name: 'nguyễn bá hưng2',
+    canNang: 40,
+    chieuCao: 160,
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  },
+  {
+    id: 4,
+    name: 'nguyễn bá hưng3',
+    canNang: 50,
+    chieuCao: 150,
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  },
+  {
+    id: 5,
+    name: 'nguyễn bá hưng4',
+    canNang: 29,
+    chieuCao: 150,
+    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+  }
+  ]
+}
