0d39577c889b02e19d874ea428b136d68818af33
hungnbph <hungnb@mobio.mobio.vn>
2022-03-09 17:10:43 +0700

    ngIf ngFor lab1

--- src/app/data.define.ts
+++ src/app/data.define.ts
@@ -13,7 +13,7 @@ export const student = () => {
     age: '23',
     phone: '0338757424',
     email: 'hung19991231@gmail.com',
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   },
   {
     id: 3,
@@ -21,7 +21,7 @@ export const student = () => {
     age: '23',
     phone: '0338757424',
     email: 'hung19991231@gmail.com',
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   },
   {
     id: 4,
@@ -29,7 +29,7 @@ export const student = () => {
     age: '23',
     phone: '0338757424',
     email: 'hung19991231@gmail.com',
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ export const student = () => {
     age: '23',
     phone: '0338757424',
     email: 'hung19991231@gmail.com',
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   }
   ]
 }
@@ -48,35 +48,35 @@ export const user = () => {
     name: 'nguyễn bá hưng',
     canNang: 35,
     chieuCao: 175,
-    avatar: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F1149121661142005209%2F&psig=AOvVaw00e0Otu3hJMX7KSDw4X3aD&ust=1646901467619000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKDT2pnQuPYCFQAAAAAdAAAAABAD'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   },
   {
     id: 2,
     name: 'nguyễn bá hưng1',
     canNang: 23,
     chieuCao: 185,
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   },
   {
     id: 3,
     name: 'nguyễn bá hưng2',
     canNang: 40,
     chieuCao: 160,
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   },
   {
     id: 4,
     name: 'nguyễn bá hưng3',
     canNang: 50,
     chieuCao: 150,
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   },
   {
     id: 5,
     name: 'nguyễn bá hưng4',
     canNang: 29,
     chieuCao: 150,
-    avatar: 'https://ipa.poly.edu.vn/user/ph/PH16698.jpg'
+    avatar: 'https://nguoi-noi-tieng.com/photo/tieu-su-ribi-sachi-9303.jpg'
   }
   ]
 }
